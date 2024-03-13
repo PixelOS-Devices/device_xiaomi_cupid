34de09db6a2036dc5db59625368aa37103398de7
bheatleyyy <sugakesshaa@pixelos.net>
2024-03-13 21:25:35 +0100

    cupid: Match stock screen density
    Change-Id: Ic1fa6df225ee2ec96ce0ffc46defc11670b4803f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,4 +20,4 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/properties/vendor.prop
 
 # Screen density
-TARGET_SCREEN_DENSITY := 440
+TARGET_SCREEN_DENSITY := 480
